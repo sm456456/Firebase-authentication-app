1a9f69a6f74a29f010169cdf04a4b6799094ea70
Selma456 <74097172+sm456456@users.noreply.github.com>
2021-07-26 18:54:14 +0200

    add buttons

---
+++ public/app.js
@@ -0,0 +1,13 @@
+///// User Authentication /////
+
+const auth = firebase.auth()
+
+const whenSignedIn = document.getElementById("whenSignedIn")
+const whenSignedOut = document.getElementById("whenSignedOut")
+
+const signInBtn = document.getElementById("signInBtn")
+const signOutBtn = document.getElementById("signOutBtn")
+
+const userDetails = document.getElementById("userDetails")
+
+const provider = new firebase.auth.GoogleAuthProvider()
